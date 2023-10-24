4573883e7dc7fa5a7893880d7561e1a3cf5d6016
CodeWizard-1 <>
2023-10-24 14:19:55 +0000

    Add summernote

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar2023-32a05ffd8610.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar2023-32a05ffd8610.herokuapp.com', '8000-codewizard-1-dlango-blog-pnab1a30k8.us2.codeanyapp.com']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
